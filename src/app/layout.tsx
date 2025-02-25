e54baeb6b86fb75824d7d14090dc39ee7c0c244e
ayush123-bit <ayushrai803@gmail.com>
2025-02-25 16:40:48 +0530

    fixed linting problem

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <script src="./node_modules/preline/dist/preline.js"></script>
+      <Script src="./node_modules/preline/dist/preline.js" strategy="afterInteractive"></Script>
 
       <body className={inter.className}>{children}</body>
     </html>
